fc559add7c32a338b8d01bc31086aa5230543f85
Gabor Koncz <gabee1987@gmail.com>
2018-02-22 15:11:04 +0100

    added giftee-form component, need service too

---
+++ GifteeWebApiAngularBasedUI/ClientApp/app/components/giftee-form/giftee-form.component.ts
@@ -0,0 +1,24 @@
+﻿import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-giftee-form',
+    templateUrl: './giftee-form.component.html',
+    styleUrls: ['./giftee-form.component.css']
+})
+export class GifteeFormComponent implements OnInit {
+
+    //constructor(private gifteeFormService: GifteeFormService) { }
+
+    users: any = [];
+    giftees: any = [];
+    giftee: any = {};
+
+
+
+
+    ngOnInit() {
+        //this.gifteeFormService.getUsers().subscribe(users =>
+        //    this.users = users);
+        //console.log("USERS", this.users);
+    }
+}
